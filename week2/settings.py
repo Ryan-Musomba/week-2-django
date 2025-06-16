a1899c995446aa15b6816ebc393630584682c9bb
Ryan-Musomba <ryanmusomba@gmail.com>
2025-06-16 20:59:25 +0300

    jk

--- week2/settings.py
+++ week2/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'week2.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600
+        
     )
 }
 
